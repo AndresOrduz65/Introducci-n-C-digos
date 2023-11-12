adaa1339c109061c8365e835c2d4e962f1961b72
AndresOrduz65 <141773190+AndresOrduz65@users.noreply.github.com>
2023-11-12 14:21:49 -0500

    Add files via upload

---
+++ Proyecto.ino
@@ -0,0 +1,52 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+#include <DHT.h>
+#define SENSOR1_PIN A0 
+#define SENSOR2_PIN A1
+#define DHTTYPE DHT11 // Tipo de sensor
+#define DHTPIN1 2 // Pin 2 del primer sensor
+#define DHTPIN2 3 // Pin 3 del segundo sensor
+DHT dht1(DHTPIN1, DHTTYPE); // Objeto para el primer sensor
+DHT dht2(DHTPIN2, DHTTYPE); // Objeto para el segundo sensor
+
+LiquidCrystal_I2C lcd(0x27,16,2);
+
+void setup() {
+  
+  Serial.begin(9600);
+
+  pinMode(SENSOR1_PIN, INPUT);
+  pinMode(SENSOR2_PIN, INPUT);
+
+  dht1.begin(); // Inicializa el primer sensor
+  dht2.begin(); // Inicializa el segundo sensor
+
+  Wire.begin();  
+  lcd.begin(16, 2);
+  lcd.clear();
+  lcd.backlight();   
+
+}
+
+void loop() {
+  int valor1 = analogRead(SENSOR1_PIN);   // Lee los valores analógicos de los pines del sensor
+  int valor2 = analogRead(SENSOR2_PIN);
+  int HumedadP = (valor1 + valor2) / 2; // Calcula el promedio de los valores
+  int H = map(HumedadP, 400, 1013, 100, 0); // Usa la función map para escalar el valor de 0 a 1023 a 0 a 100 para q el valor sea en %
+
+  float temp1 = dht1.readTemperature(); // Lee la temperatura del primer sensor
+  float temp2 = dht2.readTemperature(); // Lee la temperatura del segundo sensor
+  float T = (temp1 + temp2) / 2; // Calcula el promedio de la temperatura
+
+  lcd.setCursor(0, 0);  // Ubica el cursor en la primera posición (columna:0) de la segunda línea (fila:1)
+  lcd.print("Humedad: ");  // Escribe el texto "Humedad: "
+  lcd.print(H);  // Escribe el valor de la variable H
+  lcd.print("%");  // Escribe el símbolo de porcentaje
+  
+  lcd.setCursor(0, 1);
+  lcd.print("Temp: ");
+  lcd.print(T);
+  lcd.print(" C");
+
+  delay(1000);  // Espera un segundo
+}
